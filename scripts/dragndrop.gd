49c7f8620f8d38a1cd32f611d73ac3dd7364a4c7
DarlanApenas <darlan.alfeu@dcx.ufpb.br>
2026-02-21 19:36:45 -0300

    novas texturas

--- scripts/dragndrop.gd
+++ scripts/dragndrop.gd
@@ -1,19 +1,33 @@
 extends Node2D
-# https://www.youtube.com/watch?v=iSpWZzL2i1o
 
 var selected  = false
+var rest_point
+var rest_nodes = []
 
+func _ready():
+	rest_nodes = get_tree().get_nodes_in_group("zone")
+	rest_point = rest_nodes[0].global_position
+	rest_nodes[0].select()
 
-func _on_block_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_block_input_event(viewport: Node, event: InputEvent, shape_idx: int):
 	if Input.is_action_just_pressed("draggable_click"):
 		selected = true
 
 func _physics_process(delta: float) -> void:
 	if selected:
 		global_position = lerp(global_position,get_global_mouse_position(), 25 * delta)
+	else:
+		global_position = lerp(global_position, rest_point, 10 * delta)
 		
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 			selected = false
+			var shortest_dist = 10
+			for child in rest_nodes:
+				var distance = global_position.distance_to(child.global_position)
+				if distance < shortest_dist:
+					child.select()
+					rest_point = child.global_position
+					shortest_dist = distance
 	
--- scripts/drop_zone.gd
+++ scripts/drop_zone.gd
@@ -2,7 +2,7 @@ extends Marker2D
 
 
 func _draw():
-	draw_circle(Vector2.ZERO, 75, Color.BLANCHED_ALMOND)
+	draw_circle(Vector2.ZERO, 25, Color.BLANCHED_ALMOND)
 	
 func select():
 	for child in get_tree().get_nodes_in_group("zone"):
